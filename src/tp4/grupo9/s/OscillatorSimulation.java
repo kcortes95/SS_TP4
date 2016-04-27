d3ad944dfe59779d94d94eb33848a5187e75016a
kcortes95 <kcortes95@gmail.com>
2016-04-27 10:23:03 -0300

    Ok, ahi hice los cambios que habian que hacer. sigue sin andar.

--- src/tp4/grupo9/s/OscillatorSimulation.java
+++ src/tp4/grupo9/s/OscillatorSimulation.java
@@ -1,7 +1,6 @@
 package tp4.grupo9.s;
 
 import java.awt.Color;
-import java.util.*;
 
 public class OscillatorSimulation {
 
@@ -11,18 +10,19 @@ public class OscillatorSimulation {
 		double counter = 0;
 		double step = 0.1;
 
-		//OnScreen screen = new OnScreen(100, 800, 600);
+		Output.fileName = "simulacion.xyz";
 
-		Output.fileName = "testVerlet3.xyz";
+		double k = 10;
+		double gamma = 1;
 		
-		double force = p.m  * p.ax;
 		while (counter <= 50) {
-			Verlet2.calculateVelocity(counter, step, p, force);
-			//Verlet.nextPosition(counter, step, p, force);
+			double force = p.m * p.ax;
+			Verlet.nextPosition(counter, step, p, force);
+			//hay que actualizar la aceleracion (filmina 31)
+			//r1 es la velocidad que la saco de la filmina 11
+			p.ax = (-k*p.rx-gamma*p.vx)/p.m;
 			counter += step;
 			Output.getInstace().write(p, counter);
-
-			//screen.draw(p);
 		}
 
 	}
--- src/tp4/grupo9/s/Verlet.java
+++ src/tp4/grupo9/s/Verlet.java
@@ -5,17 +5,18 @@ public class Verlet {
 	public static double nextPosition(double initialTime, double deltaTime, Particle p, double force) {
 
 		if (initialTime == 0)
-			// lo tengo que calcular con euler
 			p.brx = p.getPossition() + deltaTime * p.getVelocity() + (Math.pow(deltaTime, 2) / (2 * p.m)) * force;
 
-		double calc1 = 2 * p.getPossition() - p.brx;
-		double calc2 = (Math.pow(deltaTime, 2) / p.m) * force;		
-		
+		double calc1 = 2 * p.rx - p.brx;
+		double calc2 = (Math.pow(deltaTime, 2) / p.m) * force;
+
+		p.vx = (p.prx - p.brx) / (2 * deltaTime);
+
 		// actualizar la posicion de la particula
 		p.brx = p.rx;
 		p.rx = p.prx;
-		//p.rx = calc1 - calc2;
 		p.prx = calc1 - calc2;
+		// p.rx = calc1 - calc2;
 
 		return p.rx;
 
